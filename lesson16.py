3fd401b4cc5098f1ff90a82fa767dc30921201f2
Nazgul1985 <nazgul110185@gmail.com>
2023-11-23 09:26:44 +0600

    Update lesson16.py

---
+++ lesson16.py
@@ -0,0 +1,10 @@
+class Vechicle():
+    def __init__(self,model, year):
+        self.model=model
+        self.year=year
+class Car (Vechicle):
+    def __init__(fuel_type):
+        super().__init__(model, year)
+
+        
+
