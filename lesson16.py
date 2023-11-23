cf4e1cc531f370fe037cd356c67af0da6088b50a
Nazgul1985 <nazgul110185@gmail.com>
2023-11-23 10:31:26 +0600

    lesson16.py

--- lesson16.py
+++ lesson16.py
@@ -1,10 +1,48 @@
-class Vechicle():
-    def __init__(self,model, year):
+class Animal(ABC):
+    @abstractmethod
+    def Proverka(self):
+        pass
+class Flyable(ABC):
+    @abstractmethod
+    def Info(self):
+        pass
+class Proiz(Animal,Flyable):
+        def Proverka(self):
+             print ('Это метод первого класса')
+        def Info(self):
+             print ('Это метод 2 класса')
+proba= Proiz()
+proba.Proverka() 
+#3 
+from abc import ABC, abstractmethod
+class Animal(ABC):
+    @abstractmethod
+    def Proverka(self):
+        pass
+
+class Vehicle(ABC):
+    @abstractmethod
+    def __init__(self, model, year):
         self.model=model
-        self.year=year
-class Car (Vechicle):
-    def __init__(fuel_type):
-        super().__init__(model, year)
+        self.year=year  
+    def Info():
+        print ('Это базовый класс')
 
-        
+class Car(Vehicle):
+    def __init__(self, model,year,fuel_type):
+        super().__init__(model,year)
+        self.fuel_type=fuel_type
 
+class Toyota(Car):
+    def __init__(self, model,year,fuel_type, model_name, fuel_efficciency):
+        super().__init__(model,year,fuel_type)
+        self.model_name=model_name
+        self.fuel_efficciency=fuel_efficciency
+    def Info():
+        print ('Это Тайота')
+class Tesla(Car):
+    def __init__(self, model,year,fuel_type, model_name, fuel_efficciency):
+        super().__init__(model,year,fuel_type, model_name, fuel_efficciency)
+    def Info():
+        print ('Это Tesla')    
+cariki = Car()
