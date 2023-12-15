becb07e7882b146babb4c1eee1020f7a8a68646e
Nazgul1985 <nazgul110185@gmail.com>
2023-12-15 19:18:17 +0600

    lesson25.py

--- chernovik.py
+++ chernovik.py
@@ -1,29 +1,9 @@
-import tkinter as tk
-root=tk.Tk()
-root.geometry("400x400")
+text = "Пример текста, который будет выводиться жирным шрифтом"
 
-def handle_checkbox():
-    selected_values = []
-    for checkbox in checkboxes:
-        if checkbox.get():
-            selected_values.append(checkbox.сget("text"))
-    res_label.config(text = selected_values)
+bold_text = "\033[1m" + text + "\033[0m"
 
-
-
-checkboxes = []
-values = ["Option 1", "Option 2", "Option 3", "Option 4"]
-
-for value in values:
-    checkbox = tk.Checkbutton(root, text=value)
-    checkbox.pack()
-    checkboxes.append(checkbox)
-
-button = tk.Button(root, text="Нажми", command=handle_checkbox)
-button.pack()
-res_label = tk.Label(root, text = ' ')
-res_label.pack()
-root.mainloop()
+print(bold_text)
+print('рпрпрп')
 
 
 
